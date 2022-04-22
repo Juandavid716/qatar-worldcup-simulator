2de3edc5974230a6c2d60e28f93df94382465c69
juandavid716 <jbojato@uninorte.edu.co>
2022-04-22 16:40:24 -0500

    🍱 Added countries json and group template

---
+++ worldcup-qatar/src/components/Group.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Group = ({ countries }) => {
+	return (
+		<div className='group'>
+			<div className='groupLetter'></div>
+			<div className='countries'>
+				<img></img>
+				<span></span>
+			</div>
+		</div>
+	);
+};
+
+export default Group;
